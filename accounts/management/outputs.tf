02c57b437cee039764668250f9e364e0e16cbc07
cloudsecgrc <cloudsecgrc@gmail.com>
2025-12-22 14:34:25 -0500

    Update outputs.tf

--- accounts/management/outputs.tf
+++ accounts/management/outputs.tf
@@ -12,13 +12,3 @@ output "guardduty_detector_id" {
   description = "GuardDuty detector ID"
   value       = data.aws_guardduty_detector.main.id
 }
-
-output "terraform_state_bucket" {
-  description = "S3 bucket for Terraform state"
-  value       = data.aws_s3_bucket.terraform_state.id
-}
-
-output "terraform_lock_table" {
-  description = "DynamoDB table for state locking"
-  value       = data.aws_dynamodb_table.terraform_lock.name
-}
