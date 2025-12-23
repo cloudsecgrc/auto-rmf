132d7775edba25d74dcd4fb2da8afc254b0480e4
cloudsecgrc <cloudsecgrc@gmail.com>
2025-12-23 12:36:19 -0500

    Update variables.tf

--- accounts/workload/variables.tf
+++ accounts/workload/variables.tf
@@ -1,11 +1,20 @@
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "management_account_id" {
   description = "Management account ID"
+  type        = string
+}
+
+variable "logging_account_id" {
+  description = "Logging account ID"
+  type        = string
+}
+
+variable "security_account_id" {
+  description = "Security account ID"
   type        = string
 }
 
@@ -14,20 +23,12 @@ variable "workload_account_id" {
   type        = string
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
+variable "organization_id" {
+  description = "AWS Organization ID"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block for public subnet"
+variable "alert_email" {
+  description = "Email address for security alerts"
   type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "private_subnet_cidr" {
-  description = "CIDR block for private subnet"
-  type        = string
-  default     = "10.0.2.0/24"
 }
