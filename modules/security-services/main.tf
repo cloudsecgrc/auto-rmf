577b2a306339f0741e03245a5cfd776bdafcfa2f
cloudsecgrc <cloudsecgrc@gmail.com>
2025-12-23 12:47:36 -0500

    Update main.tf

--- modules/security-services/main.tf
+++ modules/security-services/main.tf
@@ -1,54 +1,92 @@
+########### TERRAFORM VERSION ###########
 terraform {
   required_version = ">= 1.5.0"
 }
 
-# GuardDuty
-data "aws_guardduty_detector" "main" {}
+########### LOCAL VARIABLES ###########
+locals {
+  config_snapshots_bucket = "auto-rmf-config-snapshots"
+}
 
-# Security Hub
+##############################################################################
+##############################################################################
+
+########### GUARDDUTY DETECTOR ###########
+resource "aws_guardduty_detector" "main" {
+  enable = true
+
+  datasources {
+    s3_logs {
+      enable = true
+    }
+    kubernetes {
+      audit_logs {
+        enable = true
+      }
+    }
+    malware_protection {
+      scan_ec2_instance_with_findings {
+        ebs_volumes {
+          enable = true
+        }
+      }
+    }
+  }
+}
+
+##############################################################################
+##############################################################################
+
+########### SECURITY HUB ###########
 resource "aws_securityhub_account" "main" {}
 
+########### SECURITY HUB STANDARDS - CIS ###########
 resource "aws_securityhub_standards_subscription" "cis" {
   standards_arn = "arn:aws:securityhub:${var.aws_region}::standards/cis-aws-foundations-benchmark/v/1.4.0"
-  
-  depends_on = [aws_securityhub_account.main]
+  depends_on    = [aws_securityhub_account.main]
 }
 
+########### SECURITY HUB STANDARDS - NIST ###########
 resource "aws_securityhub_standards_subscription" "nist" {
   standards_arn = "arn:aws:securityhub:${var.aws_region}::standards/nist-800-53/v/5.0.0"
-  
-  depends_on = [aws_securityhub_account.main]
+  depends_on    = [aws_securityhub_account.main]
 }
 
-# AWS Config
+##############################################################################
+##############################################################################
+
+########### AWS CONFIG RECORDER ###########
 resource "aws_config_configuration_recorder" "main" {
   name     = "auto-rmf-config-recorder"
   role_arn = aws_iam_role.config.arn
-  
+
   recording_group {
     all_supported                 = true
     include_global_resource_types = true
   }
 }
 
+########### AWS CONFIG DELIVERY CHANNEL ###########
 resource "aws_config_delivery_channel" "main" {
   name           = "auto-rmf-config-delivery"
-  s3_bucket_name = "auto-rmf-config-snapshots"
-  
-  depends_on = [aws_config_configuration_recorder.main]
+  s3_bucket_name = local.config_snapshots_bucket
+  depends_on     = [aws_config_configuration_recorder.main]
 }
 
+########### AWS CONFIG RECORDER STATUS ###########
 resource "aws_config_configuration_recorder_status" "main" {
   name       = aws_config_configuration_recorder.main.name
   is_enabled = true
-  
   depends_on = [aws_config_delivery_channel.main]
 }
 
-# Config IAM role
+##############################################################################
+##############################################################################
+
+########### CONFIG IAM ROLE ###########
 resource "aws_iam_role" "config" {
   name = "AWSConfigRole-${var.account_id}"
-  
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -63,15 +101,17 @@ resource "aws_iam_role" "config" {
   })
 }
 
+########### CONFIG IAM ROLE POLICY ATTACHMENT ###########
 resource "aws_iam_role_policy_attachment" "config" {
   role       = aws_iam_role.config.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
 }
 
+########### CONFIG IAM ROLE POLICY - S3 ACCESS ###########
 resource "aws_iam_role_policy" "config_s3" {
   name = "ConfigS3Policy"
   role = aws_iam_role.config.id
-  
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -83,8 +123,8 @@ resource "aws_iam_role_policy" "config_s3" {
           "s3:GetObject"
         ]
         Resource = [
-          "arn:aws:s3:::auto-rmf-config-snapshots",
-          "arn:aws:s3:::auto-rmf-config-snapshots/*"
+          "arn:aws:s3:::${local.config_snapshots_bucket}",
+          "arn:aws:s3:::${local.config_snapshots_bucket}/*"
         ]
       }
     ]
