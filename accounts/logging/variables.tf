4be215875da14789bfaa1da680a08f90b715c531
cloudsecgrc <cloudsecgrc@gmail.com>
2025-12-23 12:04:58 -0500

    Update variables.tf

--- accounts/logging/variables.tf
+++ accounts/logging/variables.tf
@@ -3,12 +3,32 @@ variable "aws_region" {
   type        = string
 }
 
+variable "management_account_id" {
+  description = "Management account ID"
+  type        = string
+}
+
 variable "logging_account_id" {
   description = "Logging account ID"
   type        = string
 }
 
+variable "security_account_id" {
+  description = "Security account ID"
+  type        = string
+}
+
 variable "workload_account_id" {
   description = "Workload account ID"
+  type        = string
+}
+
+variable "organization_id" {
+  description = "AWS Organization ID"
+  type        = string
+}
+
+variable "alert_email" {
+  description = "Email address for security alerts"
   type        = string
 }
