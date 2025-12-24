1d02dab5f0560b316611abd05e0d6e6c7c1fe380
Cloud_Security <mileshall@Miless-MacBook-Pro.local>
2025-12-24 18:41:33 -0500

    fixed security hub to  delegate central

--- accounts/security/main.tf
+++ accounts/security/main.tf
@@ -312,15 +312,6 @@ resource "aws_cloudwatch_event_target" "config_noncompliant" {
 ##############################################################################
 ##############################################################################
 
-########### SECURITY HUB ORGANIZATION ADMIN ENABLEMENT ###########
-resource "aws_securityhub_organization_admin_account" "main" {
-  admin_account_id = var.security_account_id
-
-  depends_on = [
-    module.security_services
-  ]
-}
-
 ########### SECURITY HUB ORGANIZATION CONFIGURATION ###########
 resource "aws_securityhub_organization_configuration" "main" {
   auto_enable           = true
@@ -331,7 +322,7 @@ resource "aws_securityhub_organization_configuration" "main" {
   }
 
   depends_on = [
-    aws_securityhub_organization_admin_account.main
+    module.security_services
   ]
 }
 
